bd00a1df05f42703774e820a1f88a69052d13554
Ahmad Ali <Ahmad>
2024-01-11 17:42:05 +0300

    [design]: main page container designed responsively

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import './App.css';
 
+/*
+
+  todo: 1. create responsive container for 2 sizes : large and small under App Component inside of it create 2 components . side for the picture compoennt . another is for info component . percentage is 35% for 65%
+
+  todo: 2. create
+
+*/
+
 function App() {
 
   return (
     <>
-      <div className='flex flex-row text-red-500 font-bold underline'>
-        <h1>1</h1>
-        <h1>2</h1>
-        <h1>3</h1>
+      <div className='main-page pt-3 pr-3 pl-3 pb-3 w-screen h-screen bg-red-300 flex flex-col md:flex-row text-black'>
+        <div className='basis-[35%] bg-green-50'>35</div>
+        <div className='basis-[65%] bg-yellow-400'>65</div>
       </div>
     </>
   )
