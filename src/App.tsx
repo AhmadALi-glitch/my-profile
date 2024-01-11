52100d19c9e5091835406ff991cfbab07817d785
Ahmad Ali <Ahmad>
2024-01-11 22:42:37 +0300

    [theme]: add background/colors/shadow theme in the tailwind.config.js file

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import './App.css';
 
   todo: 1. create responsive container for 2 sizes : large and small under App Component inside of it create 2 components . side for the picture compoennt . another is for info component . percentage is 35% for 65%
 
-  todo: 2. create
+  todo: 2. create the separator line between the two components . and handle it's (height, width) reponsively
+
+  todo: 3.  customize theme colors in tailwind.config.js
 
 */
 
@@ -12,9 +14,22 @@ function App() {
 
   return (
     <>
-      <div className='main-page pt-3 pr-3 pl-3 pb-3 w-screen h-screen bg-red-300 flex flex-col md:flex-row text-black'>
-        <div className='basis-[35%] bg-green-50'>35</div>
-        <div className='basis-[65%] bg-yellow-400'>65</div>
+      {/* main container */}
+      <div className=' bg-secondary pt-3 pr-3 pl-3 pb-3 w-screen h-screen'>
+
+        {/* inner container */}
+        <div className="bg-primary shadow-flat h-full w-full overflow-hidden rounded-md flex items-center flex-col md:flex-row text-black">
+
+          <div className='basis-[25%] md:basis-[35%] w-full h-full'>
+            <h1 className="text-primary text-8xl">PRIMARY TEXT COLOR</h1>
+          </div>
+          <div className='basis-[0.2%] bg-secondary size-full'></div> 
+          <div className='basis-[65%] bg-secondary w-full h-full'>
+            <h1 className="text-secondary text-8xl">SECONDARY TEXT COLOR</h1>
+          </div>
+
+        </div>
+        
       </div>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+    },
+    colors: {
+      'primary': '#37371F',
+      'secondary': '#F7F9F9'
+    },
+    backgroundColor: {
+      'primary': '#F1E8B8',
+      'secondary': '#00A676',
+    },
+    boxShadow: {
+      'flat': 'rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px;',
+      'float': 'rgba(0, 0, 0, 0.1) -4px 9px 25px -6px;',
+      'soft': 'rgba(0, 0, 0, 0.1) 0px 1px 2px 0px;'
+    }
   },
   plugins: [],
 }
