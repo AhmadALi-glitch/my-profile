681c068bf8246b05ae2d8859742b98573cfab4c8
Ahmad Ali <Ahmad>
2024-01-23 01:10:21 +0300

    [content]: english experience added

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -10,7 +10,7 @@ export default function() {
 
             <h1 className="text-secondary font-handwritten xsm:text-4xl sm:text-5xl md:text-8xl">bit about me:</h1>
 
-            <div className="text-secondary pt-5 font-handwritten xsm:text-2xl sm:text-3xl md:text-4xl">I Started learning programming since 2021 .. i found that i can spent more than 10 hours playing with algorithms and creating crazy stuff with css .. I Enjoy spending time with family, friends and also the keyborad</div>
+            <div className="text-secondary pt-5 font-handwritten xsm:text-2xl sm:text-3xl md:text-4xl">I Started learning programming since 2021 .. i found that i can spent more than 10 hours playing with algorithms and creating crazy stuff with css .. I Enjoy spending time with family, friends and also the keyborad.. and i Can talk well in english </div>
 
             <div className="text-4xl pb-12  text-secondary font-handwritten">
 
