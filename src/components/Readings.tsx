2fad66db91dee4558a780835c555ef031d40bf01
Ahmad Ali <Ahmad>
2024-02-14 19:52:09 +0300

    update about me

--- src/components/Readings.tsx
+++ src/components/Readings.tsx
@@ -7,7 +7,7 @@ export default function() {
         <>
             <h1 className="text-secondary font-handwritten xsm:text-4xl sm:text-5xl md:text-8xl">Readings:</h1>
             <div className="text-secondary font-handwritten xsm:text-2xl sm:text-3xl md:text-4xl md:pt-5">
-                I Love reading books A lot .. specially when the writer share his work journey with the reader .. my favorite is <span className="underline">`The Robert C. Martin Clean Code`</span> .. it is a LEGEND
+                I Love reading books A lot .. specially when the writer share his work journey with the reader .. my favorite is <span className="underline">`The Robert C. Martin Clean Coder`</span> .. it is a LEGEND
                 <br />
                 <div className="items-center gap-4 pt-6">
                     Currently reading : 
