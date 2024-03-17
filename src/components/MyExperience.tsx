b992dcc2c75dd826d76204024b3896b10cb78c99
Ahmad Ali <newTownElectroo@gmail.com>
2024-03-17 17:29:30 +0300

    add git to my experience

--- src/components/MyExperience.tsx
+++ src/components/MyExperience.tsx
@@ -13,7 +13,8 @@ export default function() {
         'vue' : ['managing hundreds of sfc in vuejs', 'worked with popular ui libraries like quasar'],
         'css' : ['autent in creating complex shapes and ui', 'experienced with tailwind'],
         'backend': ['designing small-medium size databses', 'creating restful api\'s using expressjs framework', 'using ORM like prisma', 'deploying applications and databases', 'basic experience in graphql', 'basic experience in using linux '],
-        'ui': ['it is a hobby']
+        'ui': ['it is a hobby'],
+        'git': ['managing large codebases on git servers', 'branching, solving conflicts, stashing, configurations,ssh authentication']
     }
 
     useEffect(() => {console.log("exp")}, [exp])
@@ -36,21 +37,30 @@ export default function() {
                             borderLeft: '1px solid #eee'
                         }}>
 
-                            <FontAwesomeIcon color="orange" size="xs" icon={faHandPointer} />
+                            <FontAwesomeIcon color="#333" size="xs" icon={faHandPointer} />
                             I became well skilled in working in agile method with small to medium-size projects
+                        </div>
+
+
+                        <div onMouseOver={() => setExp('git')} className="text-2xl pt-4 pl-4 flex flex-row gap-4 items-center" style={{
+                            borderLeft: '1px solid #eee'
+                        }}>
+
+                            <FontAwesomeIcon color="#333" size="xs" icon={faHandPointer} />
+                            Skillfull Git User
                         </div>
 
                         <div onMouseOver={() => setExp('js/ts')} className="text-2xl mt-4 pl-4 flex flex-row gap-4 items-center" style={{
                             borderLeft: '1px solid #eee'
                         }}>
-                            <FontAwesomeIcon color="orange" size="xs" icon={faHandPointer} />
+                            <FontAwesomeIcon color="#333" size="xs" icon={faHandPointer} />
                             became a master at javascript and typescript</div>
 
                         <div onMouseOver={() => setExp('vue')} className="text-2xl mt-4 pl-4 flex flex-row items-center gap-4 relative" style={{
                             borderLeft: '1px solid #eee'
                         }}>
 
-                            <FontAwesomeIcon color="orange" size="xs" icon={faHandPointer} />
+                            <FontAwesomeIcon color="#333" size="xs" icon={faHandPointer} />
                             and professional in working in vuejs
 
                         </div>
@@ -58,21 +68,21 @@ export default function() {
                         <div onMouseOver={() => setExp('css')} className="text-2xl mt-4 pl-4 flex flex-row items-center gap-4 " style={{
                             borderLeft: '1px solid #eee'
                         }}>
-                            <FontAwesomeIcon color="orange" size="xs" icon={faHandPointer} />
+                            <FontAwesomeIcon color="#333" size="xs" icon={faHandPointer} />
                             Expert at css
                         </div>
 
                         <div onMouseOver={() => setExp('backend')} className="text-2xl mt-4 pl-4 flex flex-row items-center gap-4" style={{
                             borderLeft: '1px solid #eee'
                         }}>
-                            <FontAwesomeIcon color="orange" size="xs" icon={faHandPointer} />
+                            <FontAwesomeIcon color="#333" size="xs" icon={faHandPointer} />
                             gained skills in backend development in restful api using laravel and nodejs
                         </div>
 
                         <div onMouseOver={() => setExp('ui')} className="text-2xl mt-4 pl-4 flex flex-row items-center gap-4" style={{
                             borderLeft: '1px solid #eee'
                         }}>
-                            <FontAwesomeIcon color="orange" size="xs" icon={faHandPointer} />
+                            <FontAwesomeIcon color="#333" size="xs" icon={faHandPointer} />
                             UI/UX Experiences
                         </div>
                     </div>
--- src/components/Readings.tsx
+++ src/components/Readings.tsx
@@ -1,7 +1,4 @@
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faLink } from "@fortawesome/free-solid-svg-icons"
-
 export default function() {
     return (
         <>
@@ -9,10 +6,6 @@ export default function() {
             <div className="text-secondary font-handwritten xsm:text-2xl sm:text-3xl md:text-4xl md:pt-5">
                 I Love reading books A lot .. specially when the writer share his work journey with the reader .. my favorite is <span className="underline">`The Robert C. Martin Clean Coder`</span> .. it is a LEGEND
                 <br />
-                <div className="items-center gap-4 pt-6">
-                    Currently reading : 
-                    <a className="underline text-primary gap-3 flex flex-nowrap items-center" href="https://www.amazon.com/Non-Designers-Design-Book-4th/dp/0133966151">The Non-Designer's Design Book  <FontAwesomeIcon size="xs" icon={faLink} /></a>
-                </div>
             </div>
         </>
     )
