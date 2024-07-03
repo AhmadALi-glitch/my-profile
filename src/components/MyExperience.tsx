abf43c0b6030327b999c972279479313ae30a3b9
ahmadali <newTownElectroo@gmail.com>
2024-07-04 02:54:11 +0300

    solve conflicts

--- src/components/MyExperience.tsx
+++ src/components/MyExperience.tsx
@@ -1,103 +1,54 @@
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHandPointer } from "@fortawesome/free-solid-svg-icons"; 
-import { useEffect, useState } from "react";
+import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
 
 export default function() {
-
-    let [exp, setExp] = useState('agile')
-
-    let experienceDiscription: {[key: string]: string[]} = {
-        'agile' : ['working on erp systems', 'Pos, billing applications'],
-        'js/ts': ['creating typed frontend applications', 'deep knowledge in javascript and its implementaion details'],
-        'vue' : ['managing hundreds of sfc in vuejs', 'worked with popular ui libraries like quasar'],
-        'css' : ['expert at creating complex shapes and ui', 'experienced with tailwind'],
-        'backend': ['designing small-medium size databses', 'creating restful api\'s using expressjs framework', 'using ORM like prisma', 'deploying applications and databases', 'basic experience in graphql', 'basic experience in using linux '],
-        'ui': ['it is a hobby'],
-        'git': ['managing large codebases on git servers', 'branching, solving conflicts, stashing, configurations,ssh authentication']
-    }
-
-    useEffect(() => {console.log("exp")}, [exp])
 
     return (
         <>
             <h1 className="text-secondary font-handwritten xsm:text-4xl sm:text-5xl md:text-8xl">My Experiences:</h1>
+            <div className="text-secondary mt-3 font-handwritten xsm:text-2xl sm:text-3xl md:text-4xl md:pt-5">
+                after 1 year of self study i applied for a startup business solution company
+                <a href="https://www.facebook.com/UpdateCompany.sy" target="_blank" className="underline pl-2 pr-2 gap-2 ">
+                    Update
+                    <FontAwesomeIcon icon={faFacebookF} size="xs" className="ml-2 mr-2" />
+                </a>job as vueJs frontend developer . they accepted me and i worked with them for 2 years ..
 
-            <div className="text-secondary mt-3  font-handwritten xsm:text-2xl sm:text-3xl md:text-4xl md:pt-5">
-                <a href="https://www.facebook.com/UpdateCompany.sy" target="_blank" className="underline text-[30px] pl-2 pr-2 gap-2 ">
-                    i worked for 2 years and a half for a business solution company called Update as frontend engineer
-                </a>
-                        
-                <div className="wrapper flex items-center gap-3">
+                <div className="text-2xl pt-16 mt-4 pl-4 flex flex-row items-center" style={{
+                    borderLeft: '1px solid #eee'
+                }}>I became well skilled in working in agile method with small to medium-size projects</div>
 
-                    <div className="journies basis-[50%]">
+                <div className="text-2xl mt-4 pl-4 flex flex-row items-center" style={{
+                    borderLeft: '1px solid #eee'
+                }}>became a master at javascript and typescript</div>
 
+                <div className="text-2xl mt-4 pl-4 flex flex-row items-center relative" style={{
+                    borderLeft: '1px solid #eee'
+                }}>
 
-                        <div onMouseOver={() => setExp('agile')} className="text-2xl pt-16 pl-4 flex flex-row gap-4 items-center" style={{
-                            borderLeft: '1px solid #eee'
-                        }}>
-
-                            <FontAwesomeIcon color="#333" size="xs" icon={faHandPointer} />
-                            I became well skilled in working in agile method with small to medium-size projects
-                        </div>
-
-
-                        <div onMouseOver={() => setExp('git')} className="text-2xl pt-4 pl-4 flex flex-row gap-4 items-center" style={{
-                            borderLeft: '1px solid #eee'
-                        }}>
-
-                            <FontAwesomeIcon color="#333" size="xs" icon={faHandPointer} />
-                            Skillfull Git User
-                        </div>
-
-                        <div onMouseOver={() => setExp('js/ts')} className="text-2xl mt-4 pl-4 flex flex-row gap-4 items-center" style={{
-                            borderLeft: '1px solid #eee'
-                        }}>
-                            <FontAwesomeIcon color="#333" size="xs" icon={faHandPointer} />
-                            became a master at javascript and typescript</div>
-
-                        <div onMouseOver={() => setExp('vue')} className="text-2xl mt-4 pl-4 flex flex-row items-center gap-4 relative" style={{
-                            borderLeft: '1px solid #eee'
-                        }}>
-
-                            <FontAwesomeIcon color="#333" size="xs" icon={faHandPointer} />
-                            and professional in working in vuejs
-
-                        </div>
-            
-                        <div onMouseOver={() => setExp('css')} className="text-2xl mt-4 pl-4 flex flex-row items-center gap-4 " style={{
-                            borderLeft: '1px solid #eee'
-                        }}>
-                            <FontAwesomeIcon color="#333" size="xs" icon={faHandPointer} />
-                            Expert at css
-                        </div>
-
-                        <div onMouseOver={() => setExp('backend')} className="text-2xl mt-4 pl-4 flex flex-row items-center gap-4" style={{
-                            borderLeft: '1px solid #eee'
-                        }}>
-                            <FontAwesomeIcon color="#333" size="xs" icon={faHandPointer} />
-                            gained skills in backend development in restful api using laravel and nodejs
-                        </div>
-
-                        <div onMouseOver={() => setExp('ui')} className="text-2xl mt-4 pl-4 flex flex-row items-center gap-4" style={{
-                            borderLeft: '1px solid #eee'
-                        }}>
-                            <FontAwesomeIcon color="#333" size="xs" icon={faHandPointer} />
-                            UI/UX Experiences
-                        </div>
-                    </div>
-
-                    <div style={{transition: 'all ease 1s'}} className="tool-badge text-[25px] border-4 border-overflow rounded-lg p-4 basis-[50%]">
-                        {
-                            experienceDiscription[exp].map((exp) => {
-                                return <li>{exp}</li>
-                            })
-                        }
-                    </div>
+                    and professional in working in vuejs
 
                 </div>
-            </div>
+ 
+                <div className="text-2xl mt-4 pl-4 flex flex-row items-center" style={{
+                    borderLeft: '1px solid #eee'
+                }}>
+                    Expert at css
+                </div>
 
+                <div className="text-2xl mt-4 pl-4 flex flex-row items-center" style={{
+                    borderLeft: '1px solid #eee'
+                }}>
+                    gained skills in backend development in restful api using laravel and bit of nodejs
+                </div>
+
+                <div className="text-2xl mt-4 pl-4 flex flex-row items-center" style={{
+                    borderLeft: '1px solid #eee'
+                }}>
+                    UI/UX Experiences
+                </div>
+
+            </div>
         </>
     )
 }
